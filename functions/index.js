08dda66aea73f5ec48caf9925c073368c23e9938
Benjamin Iannetta <benjamin.iannetta@gmail.com>
2018-04-20 15:33:20 -0400

    Updating the Post method

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,12 @@ const functions = require('firebase-functions');
 const express = require('express');
 
 const app = express();
-app.get('/timestamp', (request, response) => {
-    response.send(`${Date.now()}`);
+app.get('/commands', (request, response) => {
+    response.send("Must be used via post");
+});
+app.post('/commands', (request, response) => {
+    const { token, text, trigger_id, user_id, command } = request.body;
+    response.send(`${user_id}`);
 });
 
 exports.app = functions.https.onRequest(app);
